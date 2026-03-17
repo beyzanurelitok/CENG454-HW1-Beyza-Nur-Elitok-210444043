dded2e5549174b9e4e40def1903f49245024565c
Beyza Nur <beyzanurelitok21@gmail.com>
2026-03-18 00:18:53 +0300

    feat: add FlightController skeleton with Rigidbody cache

---
+++ My project/Assets/Scripts/FlightController.cs
@@ -0,0 +1,35 @@
+// FlightController.cs
+// CENG 454 – HW1: Sky-High Prototype
+// Author: Beyza Nur Elitok | Student ID: 210444043
+
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = 45f;
+    [SerializeField] private float yawSpeed = 45f;
+    [SerializeField] private float rollSpeed = 45f;
+    [SerializeField] private float thrustSpeed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        HandleRotation();
+        HandleThrust();
+    }
+
+    private void HandleRotation()
+    {
+    }
+
+    private void HandleThrust()
+    {
+    }
+}
